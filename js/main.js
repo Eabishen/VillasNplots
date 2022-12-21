a13be35815e327ed5dfc33b139583dcf6effe355
Eabishen <ebi4lbe@gmail.com>
2022-12-21 12:58:39 +0530

    completed landing page, Villas, plots

--- js/main.js
+++ js/main.js
@@ -32,3 +32,29 @@ month.onclick = () => {
         yr.style.display = "none";
     });
 };
+
+// Swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+    },
+});
