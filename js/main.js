911f03fc36a42656061f599dc3e40412eecca466
Eabishen <ebi4lbe@gmail.com>
2022-12-22 14:25:42 +0530

    Added AOS animation and refactor the code

--- js/main.js
+++ js/main.js
@@ -38,6 +38,10 @@ month.onclick = () => {
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+      },
     loop: true,
     pagination: {
         el: ".swiper-pagination",
